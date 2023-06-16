1cc171a749922126dfe02a3696199119284b324a
Hoàng Văn Hoan <hoan.hoang190210@vnuk.edu.vn>
2023-06-16 22:27:27 +0700

    16/6/2023, update asets, character controller to rigibody

---
+++ Assets/Scripts/B2Controller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B2Controller : MonoBehaviour
+{
+    public float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,122 +5,123 @@ using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController controller;
-    public float moveSpeed = 2f;
-    public float sprintSpeed = 5f;
-    public float rotationSpeed = 15f;
-    public float animationBlendSpeed = 2f;
-    public float jumpSpeed = 5f;
-    public float bounceForce = 5f;
-    public float minBounceAngle = 45f;
-    public Camera cam;
+    Rigidbody rb;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float sprintSpeed = 5f;
+    [SerializeField] float rotationSpeed = 15f;
+    [SerializeField] float animationBlendSpeed = 2f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] Camera cam;
     Animator animator;
 
     float mDesiredRotation = 0f;
     float mDesiredAnimationSpeed = 0f;
     bool mSprinting = false;
 
-    public float mSpeedY = 0f;
-    public float mGravity = -9.81f;
+    [SerializeField] float mSpeedY = 0f;
+    [SerializeField] float mGravity = -9.81f;
     bool mJumping = false;
+
+    [SerializeField] float jumpForce;
+
+    [Header("Ground Check")]
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask whatIsGround;
+    bool grounded;
 
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MyInpput();
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
     }
 
-    private void MyInpput()
+    private void MyInput()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // Get the input
-        float verticalInput = Input.GetAxis("Vertical"); // Get the input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if(Input.GetButtonDown("Jump") && !mJumping)
+        if (Input.GetButtonDown("Jump") && !mJumping && grounded)
         {
             mJumping = true;
             animator.SetTrigger("Jump");
-            Debug.Log("Jump");         
-            //animator.SetBool("isJump", true);
+            Debug.Log("Jump");
 
-            mSpeedY += jumpSpeed  ; // Apply jump to the player
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            Debug.Log("Jump");
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
         else
         {
             mJumping = false;
         }
 
-        if (!controller.isGrounded)
+        if (!IsGrounded())
         {
-            mSpeedY += mGravity * Time.deltaTime; // Apply gravity to the player
+            mSpeedY += mGravity * Time.deltaTime;
         }
-        else if(mSpeedY < 0f)
+        else if (mSpeedY < 0f)
         {
             mSpeedY = 0f;
         }
 
-        animator.SetFloat("SpeedY", mSpeedY / jumpSpeed); // Set the animation speed
-        if(mJumping && mSpeedY < 0f)
+        animator.SetFloat("SpeedY", mSpeedY / jumpSpeed);
+
+        if (mJumping && mSpeedY < 0f)
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, .5f, LayerMask.GetMask("Default")))
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, .5f, LayerMask.GetMask("Ground")))
             {
                 mJumping = false;
                 animator.SetTrigger("Land");
             }
         }
 
-        mSpeedY += mGravity * Time.deltaTime; // Apply gravity to the player
-        mSprinting = Input.GetKey(KeyCode.LeftShift); // Get the input
+        mSprinting = Input.GetKey(KeyCode.LeftShift);
 
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        Vector3 rotatedMovement = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0) * movement;
+        Vector3 verticalMovement = Vector3.up * mSpeedY;
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized; // Get the movement vector
+        rb.velocity = (verticalMovement + (rotatedMovement * (mSprinting ? sprintSpeed : moveSpeed)));
 
-
-        Vector3 rotatedMovement = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0) * movement; // Rotate the movement vector to the direction of the camera
-        Vector3 verticalMovement = Vector3.up * mSpeedY; // Get the vertical movement vector
-        controller.Move((verticalMovement + (rotatedMovement * (mSprinting ? sprintSpeed : moveSpeed))) * Time.deltaTime); // Move the player
-
-        if(rotatedMovement.magnitude > 0) // If the player is moving (magnitude is the length of the vector 
+        if (rotatedMovement.magnitude > 0)
         {
-            mDesiredRotation = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg; // Get the angle of movement
-            mDesiredAnimationSpeed = mSprinting ? 1f : 0.5f; // Set the animation speed
+            mDesiredRotation = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
+            mDesiredAnimationSpeed = mSprinting ? 1f : 0.5f;
         }
         else
         {
             mDesiredAnimationSpeed = 0f;
         }
 
-        animator.SetFloat("Speed", Mathf.Lerp(animator.GetFloat("Speed"), mDesiredAnimationSpeed, animationBlendSpeed * Time.deltaTime)); // Set the animation speed
+        animator.SetFloat("Speed", Mathf.Lerp(animator.GetFloat("Speed"), mDesiredAnimationSpeed, animationBlendSpeed * Time.deltaTime));
 
-        Quaternion currentRotation = transform.rotation; 
+        Quaternion currentRotation = transform.rotation;
         Quaternion targetRotation = Quaternion.Euler(0, mDesiredRotation, 0);
         transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
-
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private bool IsGrounded()
     {
-        // Kiểm tra layer của đối tượng va chạm
-        if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Bounce"))
+        RaycastHit hit;
+        float raycastDistance = 0.1f;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
         {
-            Vector3 characterBottom = controller.bounds.center - new Vector3(0f, controller.bounds.extents.y, 0f);
-            Vector3 objectTop = hit.collider.bounds.center + new Vector3(0f, hit.collider.bounds.extents.y, 0f);
-
-            // Kiểm tra điểm dưới của nhân vật chạm vào điểm top của đối tượng
-            if (characterBottom.y >= objectTop.y)
-            {
-                // Áp dụng lực nảy lên
-                mSpeedY = bounceForce;
-            }
+            return true;
         }
+        return false;
     }
 }
